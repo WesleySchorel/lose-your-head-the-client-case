51dd42b3f8aab65cb8c404e21413d5a667ffcee6
tom-2810 <tom.poortman@hva.nl>
2023-10-06 17:26:56 +0200

    FIX reactive heading

--- src/routes/[websiteUID]/+page.svelte
+++ src/routes/[websiteUID]/+page.svelte
@@ -2,7 +2,7 @@
 	import Heading from '$lib/components/heading.svelte';
 	export let data;
 
-	let heading = {
+	$: heading = {
 		titel: data.websitesData.website.titel,
 		homepage: data.websitesData.website.homepage
 	};
